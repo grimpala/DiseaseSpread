0202dfc0d71a649eed4715efd8b0702c3a9061cf
Jacob Katzeff <jacobkatzeff@gmail.com>
2025-06-30 09:37:10 -0700

    Removed clustering logic in favor of built-in graphs in networkx; added a main function and command line support.

--- seir_simulation.py
+++ seir_simulation.py
@@ -8,7 +8,8 @@ import numpy as np
 from enum import Enum
 import itertools
 import functools
-
+import os
+import argparse
 
 class GraphOptions:
     def __init__(self, graph_type):
@@ -22,42 +23,40 @@ class GraphType(Enum):
     GEOMETRIC = 5
     POWERLAW_CLUSTER = 6
 
-def build_graph(graph_options, num_clusters, nodes_per_cluster, inter_cluster_edges, initial_infected):
-    # Initialize Graph
-
+def build_graph(graph_options, num_nodes, avg_num_edges, initial_infected):
+    """Builds a social network graph of the specified type and initializes SEIR states."""
     graph_type = graph_options.graph_type
     if graph_type == GraphType.BINOMIAL:
-        G = nx.erdos_renyi_graph(n=nodes_per_cluster, p=10/n)
+        G = nx.erdos_renyi_graph(n=num_nodes, p=10/num_nodes)
     elif graph_type == GraphType.SMALL_WORLD:
-        G = nx.watts_strogatz_graph(n=nodes_per_cluster, k=inter_cluster_edges, p=0.1)
+        G = nx.watts_strogatz_graph(n=num_nodes, k=avg_num_edges, p=0.1)
     elif graph_type == GraphType.PREFERENTIAL_ATTACHMENT:
-        G = nx.barabasi_albert_graph(n=nodes_per_cluster, k=inter_cluster_edges)
+        G = nx.barabasi_albert_graph(n=num_nodes, k=avg_num_edges)
     elif graph_type == GraphType.STOCHASTIC_BLOCK:
         # TODO, default to small world
-        G = nx.watts_strogatz_graph(n=nodes_per_cluster, k=inter_cluster_edges, p=0.1)
+        G = nx.watts_strogatz_graph(n=num_nodes, k=avg_num_edges, p=0.1)
     elif graph_type == GraphType.GEOMETRIC:
-        G = nx.random_geometric_graph(n=nodes_per_cluster, radius=0.1)
+        G = nx.random_geometric_graph(n=num_nodes, radius=0.1)
     elif graph_type == GraphType.POWERLAW_CLUSTER:
-        G = nx.powerlaw_cluster_graph(n=nodes_per_cluster, m=inter_cluster_edges, p=0.3)
+        G = nx.powerlaw_cluster_graph(n=num_nodes, m=avg_num_edges, p=0.3)
     # G = nx.Graph()
     if not nx.is_connected(G):
         G = G.subgraph(max(nx.connected_components(G), key=len)).copy()
-    cluster_nodes = []
 
     # Initialize states in Graph.
     for node in G.nodes():
         G.nodes[node]["status"] = "S"
         G.nodes[node]["quarantined"] = False
 
-    # Add one random infected node
+    # Add initial_infected random infected nodes
     for infected_node in random.sample(list(G.nodes()), initial_infected):
-        
         G.nodes[infected_node]["status"] = "I" 
         G.nodes[infected_node]["infected_days"] = 0
 
-    return G, cluster_nodes
+    return G
 
 def get_status_counts(G):
+    """Returns counts of each SEIR+Q state in the graph."""
     s_count, e_count, i_count, qe_count, qi_count, r_count = 0,0,0,0,0,0
     for node in G.nodes:
         status = G.nodes[node]["status"]
@@ -79,6 +78,7 @@ def get_status_counts(G):
     return s_count, e_count, i_count, qe_count, qi_count, r_count
 
 def simulate_step(G, status_counts, p_transmission=0.05, p_quarantine_exposed=0.1, p_quarantine_infected=0.5, exposure_days=3, recovery_days=10):
+    """Simulates one time step of SEIR+Q dynamics on the graph."""
     newly_exposed = []
     newly_infected = []
     newly_quarantined = []
@@ -122,9 +122,9 @@ def simulate_step(G, status_counts, p_transmission=0.05, p_quarantine_exposed=0.
     status_counts['R'].append(R)
     return status_counts
 
-def plot_statuses(status_counts, p_transmission, p_quarantine_exposed, p_quarantine_infected, num_clusters, exposure_days, recovery_days, nodes_per_cluster, inter_cluster_edges, fractional=False):
-    population = num_clusters * nodes_per_cluster
-    
+def plot_statuses(status_counts, p_transmission, p_quarantine_exposed, p_quarantine_infected, exposure_days, recovery_days, num_nodes, avg_num_edges, fractional=False):
+    """Plots the epidemic curves for all states."""
+
     S = np.array(status_counts['S'])
     E = np.array(status_counts['E'])
     I = np.array(status_counts['I'])
@@ -133,12 +133,12 @@ def plot_statuses(status_counts, p_transmission, p_quarantine_exposed, p_quarant
     R = np.array(status_counts['R'])
 
     # Normalize to fractions
-    S_frac = S / population
-    E_frac = E / population
-    I_frac = I / population
-    QE_frac = QE / population
-    QI_frac = QI / population
-    R_frac = R / population
+    S_frac = S / num_nodes
+    E_frac = E / num_nodes
+    I_frac = I / num_nodes
+    QE_frac = QE / num_nodes
+    QI_frac = QI / num_nodes
+    R_frac = R / num_nodes
     
     # Time axis
     timesteps = range(len(S))
@@ -156,7 +156,7 @@ def plot_statuses(status_counts, p_transmission, p_quarantine_exposed, p_quarant
                       alpha=0.8)
         plt.ylabel('Population')
     plt.xlabel('Time Step')
-    plt.title(f'p_transmission={p_transmission:.2f}, p_quarantine_exposed={p_quarantine_exposed:.1f}, p_quarantine_infected={p_quarantine_infected:.1f} exposure_days={exposure_days}, recovery_days={recovery_days}, num_clusters={num_clusters}, nodes_per_cluster={nodes_per_cluster}, inter_cluster_edges={inter_cluster_edges}')
+    plt.title(f'p_transmission={p_transmission:.2f}, p_quarantine_exposed={p_quarantine_exposed:.1f}, p_quarantine_infected={p_quarantine_infected:.1f},\n exposure_days={exposure_days}, recovery_days={recovery_days}, num_nodes={num_nodes}, avg_num_edges={avg_num_edges}')
     plt.legend(loc="upper right")
     plt.grid(True)
     plt.tight_layout()
@@ -169,19 +169,15 @@ def run_simulation(epochs,
                    p_quarantine_infected, 
                    exposure_days, 
                    recovery_days, 
-                   num_clusters, 
-                   nodes_per_cluster, 
-                   inter_cluster_edges, 
+                   num_nodes,
+                   avg_num_edges,
                    initial_infected=1,
-                   prebuilt_graph=None, 
-                   plot=False):
-    if prebuilt_graph is not None:
-        G = prebuilt_graph
-    else:
-        G, _ = build_graph(graph_options, num_clusters, nodes_per_cluster, inter_cluster_edges, initial_infected=initial_infected)
-    
-    population = num_clusters * nodes_per_cluster
-        
+                   prebuilt_graph=None,
+                   save_dir=None,
+                   do_plot=False):
+    """Runs the SEIR+Q simulation and returns the status counts."""
+    G = build_graph(graph_options, num_nodes, avg_num_edges, initial_infected)
+
     # Counts of each status at each epoch, for plotting.
     status_counts = {
         'S': [],
@@ -200,17 +196,16 @@ def run_simulation(epochs,
                                       p_quarantine_infected=p_quarantine_infected,
                                       exposure_days=exposure_days,
                                       recovery_days=recovery_days)
-    if plot:
+    if do_plot:
         display.clear_output(wait=True)
         plot_statuses(status_counts,
                       p_transmission,
                       p_quarantine_exposed,
                       p_quarantine_infected,
-                      num_clusters,
                       exposure_days,
                       recovery_days,
-                      nodes_per_cluster,
-                      inter_cluster_edges)
+                      num_nodes,
+                      avg_num_edges)
 
 def num_param_combinations(param_choices):
     return functools.reduce(lambda x,y: x * len(y), param_choices.values(), 1)
@@ -247,50 +242,36 @@ def plot_variable_params(param_choices, graph_type, epochs=80, sleep_time=0.5):
                        plot=True)
         time.sleep(sleep_time)
 
-full_param_sweep = {
-    "p_transmission": [0.4, 0.2, 0.1, 0.05],
-    "p_quarantine_exposed": [0.0, 0.1, 0.2, 0.4],
-    "p_quarantine_infected": [0.3, 0.7, 1.0],
-    "exposure_days": [2,3],
-    "recovery_days": [7,14],
-    "num_clusters": [4,8],
-    "nodes_per_cluster": [64,128],
-    "inter_cluster_edges": [8,32],
-    "initial_infected": [4],
-}
-
-transmission_dynamic_params = {
-}
-
-intervention_policy_params = {
-    "p_transmission": [0.1],
-    "p_quarantine_exposed": [0.0, 0.05, 0.1, 0.2, 0.4],
-    "p_quarantine_infected": [0.0, 0.3, 0.7, 0.9],
-    "exposure_days": [3],
-    "recovery_days": [14],
-    "num_clusters": [8],
-    "nodes_per_cluster": [128],
-    "inter_cluster_edges": [32],
-    "initial_infected": [4],
-}
-
-network_structure_params = {
-}
-
-baseline_params = {
-    "p_transmission": [0.2],
-    "p_quarantine_exposed": [0.05],
-    "p_quarantine_infected": [0.4],
-    "exposure_days": [3],
-    "recovery_days": [14],
-    "num_clusters": [8],
-    "nodes_per_cluster": [128],
-    "inter_cluster_edges": [8],
-    "initial_infected": [4],
-}
-
-graph_options = GraphOptions(GraphType.SMALL_WORLD)
-
-print(num_param_combinations(full_param_sweep))
-for _ in range(1):
-    plot_variable_params(baseline_params, GraphOptions(GraphType.POWERLAW_CLUSTER), sleep_time=1)
+def main():
+    parser = argparse.ArgumentParser(description="SEIR+Q Disease Spread Simulation on Social Networks")
+    parser.add_argument('--graph_type', type=str, default='SMALL_WORLD', choices=[g.name for g in GraphType], help='Type of network graph')
+    parser.add_argument('--num_nodes', type=int, default=256, help='Number of nodes in the network')
+    parser.add_argument('--avg_num_edges', type=int, default=8, help='Average degree/inter-cluster edges')
+    parser.add_argument('--initial_infected', type=int, default=4, help='Initial number of infected individuals')
+    parser.add_argument('--epochs', type=int, default=80, help='Number of simulation steps')
+    parser.add_argument('--p_transmission', type=float, default=0.2, help='Transmission probability per contact')
+    parser.add_argument('--p_qE', type=float, default=0.05, help='Quarantine probability for exposed')
+    parser.add_argument('--p_qI', type=float, default=0.4, help='Quarantine probability for infected')
+    parser.add_argument('--exposure_days', type=int, default=3, help='Days in exposed state before infectious')
+    parser.add_argument('--recovery_days', type=int, default=14, help='Days in infectious state before recovery')
+    parser.add_argument('--save_dir', type=str, default=None, help='Directory to save plots')
+    args = parser.parse_args()
+    if args.save_dir and not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    graph_options = GraphOptions(GraphType[args.graph_type])
+    run_simulation(
+        epochs=args.epochs,
+        graph_options=graph_options,
+        p_transmission=args.p_transmission,
+        p_quarantine_exposed=args.p_qE,
+        p_quarantine_infected=args.p_qI,
+        exposure_days=args.exposure_days,
+        recovery_days=args.recovery_days,
+        num_nodes=args.num_nodes,
+        avg_num_edges=args.avg_num_edges,
+        initial_infected=args.initial_infected,
+        do_plot=True,
+        save_dir=args.save_dir
+    )
+if __name__ == "__main__":
+    main()
